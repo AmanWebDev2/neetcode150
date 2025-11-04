8c66c9aa460a894efb282519431030ffef86b356
AmaN <webdevaman2@gmail.com>
2025-11-04 23:57:27 +0530

    better

--- array & hashing/5. top k frequent elements.py
+++ array & hashing/5. top k frequent elements.py
@@ -1,5 +1,5 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def brute(self, nums: List[int], k: int) -> List[int]:
         from collections import Counter
 
         freq = Counter(nums)  # O(n) space and time
@@ -12,3 +12,19 @@ class Solution:
         arr.sort(key=lambda x: x[1], reverse=True)  # O(nlog(n))
 
         return [item[0] for item in arr[:k]]  # O(k) space and time
+
+    def better(self, nums: List[int], k: int) -> List[int]:
+        from collections import Counter
+        import heapq
+
+        freq = Counter(nums)  # O(n)
+
+        heap = []
+        heapq.heapify(heap)
+
+        for num, occ in freq.items():  # O(n)
+            heapq.heappush(heap, (occ, num))  # O(logk)
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [num for count, num in heap]  # O(k)
